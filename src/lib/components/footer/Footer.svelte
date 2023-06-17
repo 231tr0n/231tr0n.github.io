fef69a90eef86d7a7ee241f8bda6097213a3a338
231tr0n <zeltronsrikar@gmail.com>
2023-06-17 23:33:41 +0530

    Base

---
+++ src/lib/components/footer/Footer.svelte
@@ -0,0 +1,54 @@
+<div>
+  <a target="_blank" href="mailto:zeltronsrikar@gmail.com">
+    <img alt="Gmail" src="/images/gmail.svg" />
+  </a>
+  <a href="https://www.github.com/231tr0n">
+    <img alt="Github" class="github" src="/images/github-dark.svg" />
+  </a>
+  <a target="_blank" href="https://www.gitlab.com/231tr0n">
+    <img alt="Gitlab" src="/images/gitlab.svg" />
+  </a>
+  <a target="_blank" href="https://www.linkedin.com/in/231tr0n">
+    <img alt="LinkedIn" src="/images/linkedin.svg" />
+  </a>
+  <a target="_blank" href="https://www.twitter.com/231tr0n">
+    <img alt="Twitter" src="/images/twitter.svg" />
+  </a>
+</div>
+
+<style>
+  .github {
+    content: url("/images/github-light.svg");
+  }
+
+  :global(body.light-mode) .github {
+    content: url("/images/github-dark.svg");
+  }
+
+  div {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    height: 62px;
+    padding: 5px;
+    box-sizing: border-box;
+    z-index: 100;
+    border-top: 1px solid var(--color-dark-foreground);
+    background-color: var(--color-dark-background);
+  }
+
+  :global(body.light-mode) div {
+    border-top: 1px solid var(--color-light-foreground);
+    background-color: var(--color-light-background);
+  }
+
+  img {
+    padding: 5px;
+    width: 30px;
+    height: 30px;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script>
+  import Navbar from "$lib/components/navbar/Navbar.svelte";
+  import Footer from "$lib/components/footer/Footer.svelte";
+  import "$lib/css/main.css";
+</script>
+
+<div class="body">
+  <Navbar />
+  <div class="background">
+    <div class="opacity" />
+  </div>
+  <main>
+    <slot />
+  </main>
+  <Footer />
+</div>
+
+<style>
+  .opacity {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-dark-background);
+    opacity: 90%;
+    z-index: 2;
+  }
+
+  :global(body.light-mode) .opacity {
+    background-color: var(--color-light-background);
+  }
+
+  .background {
+    background-image: url("/images/background.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    filter: blur(5px);
+    position: fixed;
+    top: 62px;
+    left: 0px;
+    right: 0px;
+    bottom: 62px;
+    z-index: 1;
+  }
+
+  main {
+    position: fixed;
+    top: 62px;
+    left: 0px;
+    right: 0px;
+    bottom: 62px;
+    padding: 0px;
+    margin: 0px;
+    overflow: hidden;
+    z-index: 3;
+  }
+
+  .body {
+    overflow: hidden;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    margin: 0px;
+    padding: 0px;
+  }
+</style>
