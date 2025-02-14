439397f391a1ef39727b80c76f28af3857380db7
231tr0n <zeltronsrikar@gmail.com>
2025-02-14 22:50:43 +0000

    revert: remove changes related to refresh option for sandbox
    This reverts commit 60e75535ef17c61e5784c75e335aad0b7e9f3b53.

--- src/lib/components/Sandbox.svelte
+++ src/lib/components/Sandbox.svelte
@@ -9,21 +9,6 @@
 	let iframeElement = $state() as HTMLElement;
 	let iframe = $state() as HTMLIFrameElement;
 	let subscriber: Unsubscriber = () => {};
-	let resetted = $state(false);
-
-	let refreshIframe = (value: string) => {
-		iframe.srcdoc = value;
-		// Below line is used for triggering iframe reload by resetting src.
-		iframe.src = iframe.src;
-	};
-
-	let reset = () => {
-		refreshIframe('');
-		resetted = true;
-		setTimeout(() => {
-			resetted = false;
-		}, 2000);
-	};
 
 	let toggleFullscreen = () => {
 		if (document.fullscreenElement) {
@@ -50,7 +35,9 @@
 
 		if (srcDocStore) {
 			subscriber = srcDocStore.subscribe((value: string) => {
-				refreshIframe(value);
+				iframe.srcdoc = value;
+				// Below line is used for triggering iframe reload by resetting src.
+				iframe.src = iframe.src;
 			});
 		}
 	});
@@ -93,35 +80,6 @@
 							</svg>
 						{/if}
 					</button>
-					{#if srcDocStore}
-						<button onclick={reset} aria-label="Reset">
-							{#if !resetted}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									fill="currentColor"
-									class="bi bi-bootstrap-reboot"
-									viewBox="0 0 16 16">
-									<path
-										d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.8 6.8 0 0 0 1.16 8z" />
-									<path
-										d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324z" />
-								</svg>
-							{:else}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									fill="currentColor"
-									class="bi bi-check2"
-									viewBox="0 0 16 16">
-									<path
-										d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
-								</svg>
-							{/if}
-						</button>
-					{/if}
 				</span>
 				<span>sandbox</span>
 			</div>
